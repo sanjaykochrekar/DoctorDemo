b13a0e61301f068063495650216d72b621f3af6b
sanjay <kochrekarsanjay87@gmail.com>
2023-09-18 22:25:22 +0530

    initial setup

---
+++ DoctorDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoctorDemo
+//
+//  Created by Sanju on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
